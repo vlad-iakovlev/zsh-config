3ea96befcc69a070c4f358a2b8bfc5a2a9ff7e36
Vladislav Yakovlev <vlad309523@gmail.com>
2019-12-16 16:31:15 +0300

    Add rebase with autosquash aliases

--- zsh-config.sh
+++ zsh-config.sh
@@ -39,6 +39,10 @@ alias grbod="git rebase origin/dev"
 alias grbom="git rebase origin/master"
 alias grbud="git rebase upstream/dev"
 alias grbum="git rebase upstream/master"
+alias grbsod="GIT_SEQUENCE_EDITOR=touch git rebase -i --autosquash origin/dev"
+alias grbsom="GIT_SEQUENCE_EDITOR=touch git rebase -i --autosquash origin/master"
+alias grbsud="GIT_SEQUENCE_EDITOR=touch git rebase -i --autosquash upstream/dev"
+alias grbsum="GIT_SEQUENCE_EDITOR=touch git rebase -i --autosquash upstream/master"
 alias gff="gf origin && gf upstream && gf origin --no-prune"
 
 # Docker
