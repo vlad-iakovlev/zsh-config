a05934220080ebde665495f64fe106432f00e2f3
Vladislav Yakovlev <vlad309523@gmail.com>
2022-04-06 15:21:16 +0400

    Update aliases.zsh

--- aliases.zsh
+++ aliases.zsh
@@ -1,17 +1,11 @@
 # Git
 alias gcm="git commit -m"
-alias gcood="git checkout origin/develop"
-alias gcoom="git checkout origin/master"
-alias gcoud="git checkout upstream/develop"
-alias gcoum="git checkout upstream/master"
-alias grbod="git rebase origin/develop"
-alias grbom="git rebase origin/master"
-alias grbud="git rebase upstream/develop"
-alias grbum="git rebase upstream/master"
-alias grbsod="GIT_SEQUENCE_EDITOR=touch git rebase -i --autosquash origin/develop"
-alias grbsom="GIT_SEQUENCE_EDITOR=touch git rebase -i --autosquash origin/master"
-alias grbsud="GIT_SEQUENCE_EDITOR=touch git rebase -i --autosquash upstream/develop"
-alias grbsum="GIT_SEQUENCE_EDITOR=touch git rebase -i --autosquash upstream/master"
+alias gcood="git checkout origin/development"
+alias gcoud="git checkout upstream/development"
+alias grbod="git rebase origin/development"
+alias grbud="git rebase upstream/development"
+alias grbsod="GIT_SEQUENCE_EDITOR=touch git rebase -i --autosquash origin/development"
+alias grbsud="GIT_SEQUENCE_EDITOR=touch git rebase -i --autosquash upstream/development"
 alias gff="gf origin && gf upstream && gf origin --no-prune"
 
 # Docker
