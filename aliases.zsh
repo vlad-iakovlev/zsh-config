3c0296f032b8ff94fd4f90c22e2d840d5717a5cf
Vladislav Yakovlev <vlad309523@gmail.com>
2022-07-18 17:55:35 +0400

    Add configurable git aliases

--- aliases.zsh
+++ aliases.zsh
@@ -1,8 +1,5 @@
 # Git
 alias gcm="git commit -m"
-alias gcood="git checkout origin/development"
-alias grbod="git rebase origin/development"
-alias grbsod="GIT_SEQUENCE_EDITOR=touch git rebase -i --autosquash origin/development"
 
 for short branch in "${(@kv)GIT_BRANCH_ALIASES}"; do
   alias gco$short="git checkout $branch"
