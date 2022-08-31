e8f5891a65f93771d5d40fd01faf5bedf900290f
Vladislav Yakovlev <vlad309523@gmail.com>
2022-08-31 16:58:09 +0400

    Add git merge to aliases

--- aliases.zsh
+++ aliases.zsh
@@ -3,6 +3,7 @@ alias gcm="git commit -m"
 
 for short branch in "${(@kv)GIT_BRANCH_ALIASES}"; do
   alias gco$short="git checkout $branch"
+  alias gm$short="git merge $branch"
   alias grb$short="git rebase $branch"
   alias grbs$short="GIT_SEQUENCE_EDITOR=touch git rebase -i --autosquash $branch"
 done
